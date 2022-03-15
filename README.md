42aac5254fe2602c71f201b79fc1c915935a80f2
Icexes <dzianis.subach@itechart-group.com>
2022-03-15 21:53:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-odp6vx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-odp6vx)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products, Product } from '../products';
+import { CartService } from '../cart.service';
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: undefined | Product;
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('product added to cart');
+  }
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+}
